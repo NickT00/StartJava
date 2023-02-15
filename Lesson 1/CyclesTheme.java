a5c63a9a7e763e3c38d3c83ffc97c08baeee3f4d
NickT00 <trnik95@yandex.ru>
2023-02-15 16:58:43 +0300

    Complete 2 task

--- Lesson 1/CyclesTheme.java
+++ Lesson 1/CyclesTheme.java
@@ -14,5 +14,29 @@ public class CyclesTheme {
         } while(num == 21);
         System.out.println("In the interval [" + num + ", 21] sum of even numbers = " 
                 + even + ", but odd numbers = " + odd);
+
+        System.out.println("\n2. Вывод чисел в интервале (min и max) в порядке убывания");
+        int num1 = 10;
+        int num2 = 5;
+        int num3 = -1;
+        int max;
+        int min;
+        if(num1 > num2 & num1 > num3) {
+            max = num1;
+        } else if(num2 > num3 & num2 > num1) {
+            max = num2;
+        } else {
+            max = num3;
+        }
+        if(num1 < num2 & num1 < num3) {
+            min = num1;
+        } else if(num2 < num3 & num2 < num1) {
+            min = num2;
+        } else {
+            min = num3;
+        }
+        for(int i = max; i >= min; i--) {
+            System.out.print(i + " ");
+        }
     }
 }
