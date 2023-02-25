fdded5a370338f85e1c2be5fbb6ec1c8e95e9863
NickT00 <trnik95@yandex.ru>
2023-02-25 16:15:54 +0300

    The game Guess Number v.1.0

---
+++ Lesson 1/GuessNumber.java
@@ -0,0 +1,16 @@
+public class GuessNumber {
+    public static void main(String[] args) {
+        int computersNum = 75;
+        int playersNum = 50;
+        do {
+            if(playersNum < computersNum) {
+                System.out.println(playersNum + " is less than computer's number");
+                playersNum++;
+            } else {
+                System.out.println(playersNum + " is bigger than computer's number");
+                playersNum--;
+            }
+        } while(computersNum != playersNum);
+        System.out.println("You won!");
+    }
+}
