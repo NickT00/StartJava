c81d41c8f83266653c8eae3094509a264e7759ba
NickT00 <trnik95@yandex.ru>
2023-02-25 16:21:33 +0300

    Calculator v.1.0

---
+++ Lesson 1/MyFirstApp.java
@@ -0,0 +1,26 @@
+public class MyFirstApp {
+    public static void main(String[] args) {
+        int numA = 5;
+        int numB = 10;
+        char sign = '%';
+        int result = 0;
+        if(sign == '+') {
+            result = numA + numB;
+        } else if(sign == '-') {
+            result = numA - numB;
+        } else if(sign == '*') {
+            result = numA * numB;
+        } else if(sign == '/') {
+            result = numA / numB;
+        } else if(sign == '^') {
+            int tempNumA = numA;
+            for(int i = 1; i < numB; i++) {
+                tempNumA *= numA;
+                result = tempNumA;
+            }
+        } else {
+            result = (int) (((double) numA / (double) numB) * 100);
+        }
+        System.out.println(numA + " " + sign + " " + numB + " = " + result);
+    }
+}
