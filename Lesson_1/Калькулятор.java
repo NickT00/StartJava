0e24472d3906741a4dd7ac60be26b85a7e6f9edd
NickT00 <trnik95@yandex.ru>
2023-03-01 15:27:29 +0300

    Renamed, A, B замени на 1,2

---
+++ Lesson_1/Калькулятор.java
@@ -0,0 +1,25 @@
+public class Калькулятор {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 9;
+        char sign = '%';
+        int result = 0;
+        if(sign == '+') {
+            result = num1 + num2;
+        } else if(sign == '-') {
+            result = num1 - num2;
+        } else if(sign == '*') {
+            result = num1 * num2;
+        } else if(sign == '/') {
+            result = num1 / num2;
+        } else if(sign == '^') {
+            result = num1;
+            for(int i = 1; i < num2; i++) {
+                result *= num1;
+            }
+        } else {
+            result = num1 % num2;
+        }
+        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    }
+}
