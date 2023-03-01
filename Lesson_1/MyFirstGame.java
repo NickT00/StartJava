9b774a9372299d2f728c0f04997020ddfb0daf39
NickT00 <trnik95@yandex.ru>
2023-03-01 13:53:24 +0300

    Renamed computersNum/playersNum, removed s

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,16 +1,16 @@
 public class MyFirstGame {
     public static void main(String[] args) {
-        int computersNum = 75;
-        int playersNum = 50;
+        int computerNum = 75;
+        int playerNum = 50;
         do {
-            if(playersNum < computersNum) {
-                System.out.println(playersNum + " is less than computer's number");
-                playersNum++;
+            if(playerNum < computerNum) {
+                System.out.println(playerNum + " is less than computer's number");
+                playerNum++;
             } else {
-                System.out.println(playersNum + " is bigger than computer's number");
-                playersNum--;
+                System.out.println(playerNum + " is bigger than computer's number");
+                playerNum--;
             }
-        } while(computersNum != playersNum);
+        } while(computerNum != playerNum);
         System.out.println("You won!");
     }
 }
