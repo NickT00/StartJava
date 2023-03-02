59064e4ed88111320c0d5c021e36d77638b9d399
NickT00 <trnik95@yandex.ru>
2023-03-02 17:01:49 +0300

    Created a class Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String sex = "not binary";
+    String name = "Poll";
+    int height = 178;
+    int weight = 77;
+    int age = 17;
+
+    void move() {
+        System.out.println("moving");
+    }
+
+    void sitDown() {
+        System.out.println("sat down");
+    }
+
+    void run() {
+        System.out.println("running");
+    }
+
+    void speak() {
+        System.out.println("saying");
+    }
+
+    void learnJava() {
+        System.out.println("learning Java");
+    }
+}
