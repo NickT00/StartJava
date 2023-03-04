437481e35e5ba5a29ff90e3a655b628a7c4d5dbf
NickT00 <trnik95@yandex.ru>
2023-03-04 11:33:01 +0300

    Added getters and setters

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,28 +1,72 @@
 public class Wolf {
 
-    String sex;
-    String nickname;
-    int weight;
-    int age;
-    String color;
+    private String sex;
+    private String nickname;
+    private int weight;
+    private int age;
+    private String color;
 
-    void move() {
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age > 8) {
+            System.out.println("Incorrect age");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void move() {
         System.out.println("moving");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("sitting");
     }
 
-    void run() {
+    public void run() {
         System.out.println("running");
     }
 
-    void howl() {
+    public void howl() {
         System.out.println("howling");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("hunting");
     }
 }
