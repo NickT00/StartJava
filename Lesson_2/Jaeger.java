f6f20532f847eecfc8eba890e00b972a7b6616f1
NickT00 <trnik95@yandex.ru>
2023-03-06 17:50:27 +0300

    Implement only parameters and gets/sets

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,66 @@
+public class Jaeger {
+
+    private String name;
+    private String origin;
+    private int height;
+    private int weight;
+    private int speed;
+    private int strength;
+    private int armor;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+}
