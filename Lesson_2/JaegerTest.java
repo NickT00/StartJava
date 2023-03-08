17ab0733eec76581d286b1c8d60ba7309f08a703
NickT00 <trnik95@yandex.ru>
2023-03-08 18:12:16 +0300

    Fixed class JaegerTest

--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -2,13 +2,14 @@ public class JaegerTest {
 
     public static void main(String[] args) {
         Jaeger jaeger1 = new Jaeger();
-        Jaeger jaeger2 = new Jaeger("Crimson Typhoon", "China", 1722, 9, 8);
         jaeger1.setName("Cherno Alpha");
         jaeger1.setOrigin("Russia");
         jaeger1.setWeight(2412);
         jaeger1.setSpeed(3);
         jaeger1.setStrength(10);
         jaeger1.move();
+
+        Jaeger jaeger2 = new Jaeger("Crimson Typhoon", "China", 1722, 9, 8);
         jaeger2.move();
         System.out.println(jaeger1.getName() + " " + jaeger1.getWeight() + " tons");
         jaeger1.shoot();
